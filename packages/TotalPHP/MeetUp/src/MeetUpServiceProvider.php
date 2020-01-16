426ae09eebbdb29724c707d6d65d090dba942623
Murilo Santos <mgsantos@artit.com.br>
2020-01-16 15:10:26 -0300

    Inicio Service Provider e informacao adicional composer

---
+++ packages/TotalPHP/MeetUp/src/MeetUpServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TotalPHP\MeetUp;
+
+/**
+ * Class MeetUpServiceProvider
+ * @package   TotalPHP\MeetUp
+ * @author    Murilo Santos <murilorcbr@gmail.com>
+ */
+class MeetUpServiceProvider
+{
+
+}

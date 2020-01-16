6f6373ee377004116b4a60dd485d911c2aef6241
Murilo Santos <mgsantos@artit.com.br>
2020-01-16 16:07:29 -0300

    Demonstração de configs

--- packages/TotalPHP/MeetUp/src/MeetUpServiceProvider.php
+++ packages/TotalPHP/MeetUp/src/MeetUpServiceProvider.php
@@ -2,12 +2,30 @@
 
 namespace TotalPHP\MeetUp;
 
+use Illuminate\Support\ServiceProvider;
+
 /**
  * Class MeetUpServiceProvider
  * @package   TotalPHP\MeetUp
  * @author    Murilo Santos <murilorcbr@gmail.com>
  */
-class MeetUpServiceProvider
+class MeetUpServiceProvider extends ServiceProvider
 {
 
+    public function boot()
+    {
+        $this->publishes(
+            [
+                __DIR__ . '/../config/meetup.php' => config_path('meetup.php'),
+            ]
+        );
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/meetup.php',
+            'meetup'
+        );
+    }
 }
